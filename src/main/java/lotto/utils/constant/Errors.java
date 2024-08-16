d45de90e6179c0b8c22fa093cbf3d40d32380508
junho <>
2024-08-16 17:38:55 +0900

    refactor: 오류 메시지 enum 분리

---
+++ src/main/java/lotto/utils/constant/Errors.java
@@ -0,0 +1,18 @@
+package lotto.utils.constant;
+
+public enum Errors {
+    ERROR("[ERROR] "),
+    ONLY_CONTAIN_NUMBERS_ERROR_MSG(ERROR+"가격에는 문자열이 포함될 수 없습니다."),
+    CANNOT_DIV_BY_UNIT_PRICE_ERROR_MSG(ERROR+"가격은 1000원 단위로 입력해야 합니다.");
+
+    private final String string;
+
+    Errors (String string) {
+        this.string = string;
+    }
+
+    @Override
+    public String toString() {
+        return string;
+    }
+}
