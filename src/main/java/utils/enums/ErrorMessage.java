4f6f3ed26bc66796f02bcf4eaba86fa456ad9d92
jmd5314 <jmd5314@gmail.com>
2024-08-16 18:49:53 +0900

    refactor: 에러 메세지 중복 간소화

--- src/main/java/utils/enums/ErrorMessage.java
+++ src/main/java/utils/enums/ErrorMessage.java
@@ -1,11 +1,13 @@
 package utils.enums;
 
 public enum ErrorMessage {
-    INVALID_NUMBER("[ERROR] 숫자를 입력해주세요"),
-    INVALID_UNIT("[ERROR] 1000원 단위로 입력해주세요"),
-    INVALID_RANGE("[ERRPR] 1 ~ 45 사이의 숫자만 입력해주세요"),
-    DUPLICATE_NUMBER("[ERROR] 중복되지 않는 숫자들만 입력해주세요"),;
+    INVALID_NUMBER("숫자를 입력해주세요"),
+    INVALID_UNIT("1000원 단위로 입력해주세요"),
+    INVALID_RANGE("1 ~ 45 사이의 숫자만 입력해주세요"),
+    DUPLICATE_NUMBER("중복되지 않는 숫자들만 입력해주세요"),
+    ;
 
+    private static final String ERROR_PREFIX = "[ERROR] ";
     private final String message;
 
     ErrorMessage(String message) {
@@ -14,6 +16,6 @@ public enum ErrorMessage {
 
     @Override
     public String toString() {
-        return message;
+        return ERROR_PREFIX + message;
     }
 }
